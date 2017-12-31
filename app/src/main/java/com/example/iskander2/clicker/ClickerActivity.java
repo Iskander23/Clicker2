7d1467bbea093c5520b7df02f7e438e7283d1671
Iskander Mamleev <iskander.mamleev23@yandex.ru>
2017-12-31 15:28:43 +0600

    Final Version

--- app/src/main/java/com/example/iskander2/clicker/ClickerActivity.java
+++ app/src/main/java/com/example/iskander2/clicker/ClickerActivity.java
@@ -1,64 +1,54 @@
 package com.example.iskander2.clicker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.util.Log;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Map;
-import java.util.Set;
+import java.util.Random;
 
 
 public class ClickerActivity extends AppCompatActivity {
 
-
-    int clicks =0;
+    public static int clicks =0;
     int i=1;
     static final String APP_PREFERENCES_CLICKS = "Clicks";
     static final String APP_PREFERENCES = "Settings";
-    private SharedPreferences mClicks ;
+    static protected SharedPreferences mClicks ;
 
-    public final String TAG = "MyTest";
-    private ProgressBar progressBar;
-    volatile int progress = 0;
-    private TextView score;
-    private volatile ImageView coinView;
+    protected final String TAG = "MyTest";
+    protected static ProgressBar progressBar;
+    protected static int progress = 0;
+    protected static TextView score;
+    private ImageView coinView;
     TextView result1;
 
-    public boolean isWin = false;
-    volatile boolean isPaused;
-    volatile boolean isRestarted;
+    public static boolean isWin;
+    protected static boolean Win;
     PopupWindow popupWindow;
-    MediaPlayer backgroundMusic;
+    protected static MediaPlayer backgroundMusic;
 
     LinearLayout mainLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mClicks = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicker);
-
+        mClicks = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         score = (TextView) findViewById(R.id.score);
-        coinView = (ImageView) findViewById(R.id.imageView);
+
 
         final LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.popup_window, null);
@@ -67,99 +57,97 @@ public class ClickerActivity extends AppCompatActivity {
         boolean focusable = true;
         popupWindow = new PopupWindow(popupView,width,height,focusable);
 
-
         //Spawning the Coin
-        new Thread(new Runnable() {
+
+        final Thread spawnThread = new Thread(new Runnable() {
             @Override
             public void run() {
+                coinView = (ImageView) findViewById(R.id.imageView);
+                spawnCoin();
                 coinView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         clicks = Integer.parseInt(score.getText().toString());
                         score.setText(Integer.toString(++clicks));
                         coinView.setVisibility(View.INVISIBLE);
-                        coinView.setX((float) Math.random() * 1000);
-                        coinView.setY((float) Math.random() * 1000);
-                        Log.d(TAG, "Current cointView X " + coinView.getX());
-                        Log.d(TAG, "Current cointView Y " + coinView.getY());
-
+                        spawnCoin();
                         new Handler().postDelayed(new Runnable() {
                             @Override
                             public void run() {
                                 coinView.setVisibility(View.VISIBLE);
                             }
-                        }, 1000);
+                        },500);
                     }
                 });
             }
-        }).start();
+        });
+        spawnThread.start();
 
         //Updating ProgressBar
         Thread progressBarThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (!isWin) {
-                    try{
-                    Thread.sleep(1000);
-                    progressBar.setProgress(progress++);}
-                    catch (Exception e){}
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        progressBar.setProgress(progress++);
+                    } catch (Exception e) {
+                    }
                 }
             }
         });
         progressBarThread.start();
-
         //Checking for Win or Lose
         new Thread(new Runnable() {
             @Override
             public void run() {
-                final View view = inflater.inflate(R.layout.popup_window, null);
+                final Intent intent = new Intent(ClickerActivity.this, PopActivity.class);
+                 View view = getLayoutInflater().inflate(R.layout.popup_window, null);
                 result1 = (TextView) view.findViewById(R.id.result);
-                mainLayout = (LinearLayout) findViewById(R.id.linearLayout);
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        while (!isWin) {
-                            if (progress == progressBar.getMax()) {
+                while(true) {
+                    while (!isWin) {
+                        if (progress == progressBar.getMax()) {
+                            isWin = true;
+                            Win = false;
+                            startActivity(intent);
+                            break;
+                        } else {
+                            if (clicks == 5) {
                                 isWin = true;
-                                String string = new String();
-                                string = "Oh no! You Lost!";
-                                result1.setText(string);
-                                popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
-                                MediaPlayer loseSound = MediaPlayer.create(ClickerActivity.this, R.raw.losesound);
-                                loseSound.start();
-                            }
-                            else {
-                                if (clicks == 5) {
-                                    isWin = true;
-                                    Button button;
-                                    button = view.findViewById(R.id.restart_button);
-                                    new Handler().postDelayed(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
-                                        }
-                                    }, 100);
-                                    MediaPlayer winSound = MediaPlayer.create(ClickerActivity.this, R.raw.winsound);
-                                    winSound.start();
-                                    button.setOnClickListener(new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View view) {
-                                            popupWindow.dismiss();
-                                            clicks = 0;
-                                            progressBar.setProgress(0);
-                                        }
-                                    });
-                                }
+                                Win = true;
+                                startActivity(intent);
+                                break;
                             }
                         }
                     }
-                });
+                }
             }
         }).start();
-
+    }
+    protected void spawnCoin(){
+        Random r = new Random();
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int X = r.nextInt(displayMetrics.widthPixels);
+        int Y = r.nextInt(displayMetrics.heightPixels);
+                if(X>100){
+                    coinView.setX(X-100);
+                    coinView.setY(Y);
+                }
+                if(Y>100){
+                    coinView.setX(X);
+                    coinView.setY(Y-100);
+                }
+        if (X<100 && Y<100){
+            coinView.setX(X);
+            coinView.setY(Y);
+        }
+        Log.d(TAG, "Current cointView X " + coinView.getX());
+        Log.d(TAG, "Current cointView Y " + coinView.getY());
     }
 
     @Override
     protected void onStart() {
+        mClicks.edit().clear().apply();
         super.onStart();
         backgroundMusic = MediaPlayer.create(this,R.raw.background);
         backgroundMusic.start();
@@ -169,7 +157,8 @@ public class ClickerActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         backgroundMusic.stop();
-        popupWindow.dismiss();
+
+
         SharedPreferences.Editor editor = mClicks.edit();
         editor.putInt(APP_PREFERENCES_CLICKS,clicks);
         editor.apply();
@@ -178,23 +167,13 @@ public class ClickerActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
         backgroundMusic.start();
-
         final LinearLayout mainLayout = (LinearLayout) findViewById(R.id.linearLayout);
-
-        Log.d(TAG, "isPaused is "+ isPaused);
-                if(i!=1) {
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            popupWindow.showAtLocation(mainLayout,Gravity.CENTER, 0, 0);
-                            isPaused = false;
-                        }
-                    },100);
-                }
-
-        Log.d(TAG, "mClicks is"+ mClicks);
+           //     if(i!=1) {
+          //                  popupWindow.showAtLocation(mainLayout,Gravity.CENTER, 0, 0);
+          //                  isPaused = false;
+          //     }
             if (mClicks.contains(APP_PREFERENCES_CLICKS)) {
                 score.setText(Integer.toString(mClicks.getInt(APP_PREFERENCES_CLICKS, clicks)));
                 clicks = mClicks.getInt(APP_PREFERENCES_CLICKS, 0);
--- app/src/main/java/com/example/iskander2/clicker/PopActivity.java
+++ app/src/main/java/com/example/iskander2/clicker/PopActivity.java
@@ -1,13 +1,87 @@
 package com.example.iskander2.clicker;
 
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class PopActivity extends AppCompatActivity {
-
+    protected final String TAG = "MyTest";
+    Button button;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pop);
+        setContentView(R.layout.popup_window);
+        result = findViewById(R.id.result);
+        button = findViewById(R.id.restart_button) ;
+
+        final MediaPlayer loseSound = MediaPlayer.create(PopActivity.this, R.raw.losesound);
+        final MediaPlayer winSound = MediaPlayer.create(PopActivity.this, R.raw.winsound);
+
+       if (ClickerActivity.Win == true){
+           Log.d(TAG, "Starting win scenario");
+           winSound.start();
+           runOnUiThread(new Runnable() {
+               @Override
+               public void run() {
+                   button.setOnClickListener(new View.OnClickListener() {
+                       @Override
+                       public void onClick(View view) {
+                           restart();
+                           ClickerActivity.isWin = false;
+                           winSound.stop();
+                           Log.d(TAG, "Trying to start background music");
+                           ClickerActivity.backgroundMusic.start();
+                           finish();
+                       }
+                   });
+               }
+           });
+       }
+       if(ClickerActivity.Win == false) {
+            Log.d(TAG, "Starting lose scenario");
+            result.setText("Oh no! You Lost!");
+            loseSound.start();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            restart();
+                            loseSound.stop();
+                            ClickerActivity.backgroundMusic.start();
+                            finish();
+                        }
+                    });
+                }
+            });
+        }
+    }
+    protected void restart(){
+        ClickerActivity.clicks = 0;
+        ClickerActivity.score.setText("0");
+        ClickerActivity.progress = 0;
+        ClickerActivity.backgroundMusic.start();
+        ClickerActivity.mClicks.edit().clear().apply();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        getWindow().setLayout((int)(width*.5),(int)(height*.5));
     }
 }
